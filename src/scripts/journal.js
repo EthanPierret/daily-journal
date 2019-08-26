f63b0dd1486ebe15732a37658addb15b57e659b6
WitheredFlame <witheredspam@gmail.com>
2019-08-26 13:34:04 -0500

    Added hard coded entries

---
+++ src/scripts/journal.js
@@ -0,0 +1,37 @@
+
+
+const journalEntries = {
+
+journalEntry1 = {
+    date = "8.16.19",
+    conceptscovered = "HTML and CSS",
+    entry = "This class moves fast, already were doing advanced styling with somthing similar to float: flexbox",
+    mood = "Great",
+
+
+
+},
+
+journalEntry2 = {
+    date = "8.24.19",
+    conceptscovered = "Website presentation",
+    entry = "Did not get my website to it's best, probally becuase i was preoccupied helping others",
+    mood = "Sad",
+
+
+
+},
+
+journalEntry3 = {
+    date = "8.26.19",
+    conceptscovered = "JavaScript",
+    entry = "Javascript can do either primitive or advanced varaibles",
+    mood = "Good",
+
+
+
+},
+
+
+}
+
