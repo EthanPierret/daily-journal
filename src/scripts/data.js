48f734dbf66fa7123d898e593888b347dfc71cfa
WitheredFlame <witheredspam@gmail.com>
2019-09-03 15:08:01 -0500

    Changed methods to required ones per the instructions

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,12 +4,8 @@ const rawdatahandler = {
 getrawdata: function() {
     
 
-    fetch("http://localhost:3000/entries").then(entrieslist => entrieslist.json())
-   .then(parsentries => {
-       
-       continueloading(parsentries); // all other function after the data comes in
-       
-   });
+   return fetch("http://localhost:3000/entries").then(entrieslist => entrieslist.json())
+   
   
    
 }
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -4,15 +4,27 @@ let dom = domeditor;
 
 
 
-data.getrawdata(); // start the process while other parts of page load
+
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+*/
+
+
+data.getrawdata().then(data => continueloading(data)); // wait for then pass data to continue program
 
 
 
 
 function continueloading(jentries) { // called after the data comes in
 
+
+
 console.log(jentries.length);
 console.log(jentries);
+
+
 
 for (let i = 0; i < jentries.length; i++) { // loop through all entries
 
