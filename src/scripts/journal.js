d8ced1bd062c95900a3501dcf473a1bba8003a41
WitheredFlame <witheredspam@gmail.com>
2019-09-10 15:57:14 -0500

    Input validation + Submit button functionality

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -19,6 +19,7 @@ let dom = domeditor;
 */
 
 
+
 data.getrawdata().then(data => continueloading(data)); // wait for then pass data to continue program
 
 
@@ -41,3 +42,92 @@ for (let i = 0; i < jentries.length; i++) { // loop through all entries
 }
 
 }
+
+
+ function saveJournalEntry(entry) {
+
+
+    
+    console.log("Posting:");
+    console.log(data);
+
+    let pastentries = fetch('http://localhost:3000/entries')
+    .then(
+    
+        fetch('http://localhost:3000/entries', { // save to list "entries"
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+                },
+        body: pastentries + JSON.stringify(entry) // save the data to file
+        })
+    ).then(addSingleElement(entry)
+
+       
+
+    );
+
+ }
+
+
+
+// add a single element so the entire page does not have to reload every time i add a entry
+ function addSingleElement(element) {
+
+    let entryAsHTML = form.makeJournalEntryComponent(element); // make a contained div
+    console.log(element);
+    dom.parsedentries(entryAsHTML); // append to the dom
+
+ }
+
+
+ //clears the entries and framework
+ function clearEntries() {
+    document.querySelector(".entryLog").innerHTML = ``;
+ }
+
+
+
+
+function validateForm() {
+    if (document.querySelector("#journalDate").value === `undefined`) {
+
+        alert("Must have a Date");
+
+    } else if (document.querySelector("#journalEntry").value === `undefined`) {
+
+        alert("Must have a Entry");
+
+    } else {
+
+        let data =  { // get journal data from html
+        
+            journalDate : document.querySelector("#journalDate").value,
+            concpetsCovered : document.querySelector("#concpetsCovered").value,
+            journalEntry : document.querySelector("#journalEntry").value,
+            mood : document.querySelector("#mood").value,
+        
+        
+                    
+    
+                };
+            saveJournalEntry(data);
+
+    }
+}
+
+
+
+
+
+
+//add submit button functionality
+
+document.querySelector('#submit').addEventListener( "click", event => {
+
+    validateForm();
+                
+
+}); 
+
